0aa764a0afbd26454beccd66bb64b3d57ab3fc21
Jasoncrawforddesign <jasoncrawford.design@gmail.com>
2017-10-09 17:03:09 +1100

    Minor obstacle changes and scene value changes

--- Grey Box FinalProject 001/Assets/RockCandyRacing/Scripts/Obstacle Scripts/RocketUpdate.cs
+++ Grey Box FinalProject 001/Assets/RockCandyRacing/Scripts/Obstacle Scripts/RocketUpdate.cs
@@ -6,6 +6,7 @@ public class RocketUpdate : MonoBehaviour {
 
     Vector3 direction;
     public float speed;
+    float deadTime = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +15,15 @@ public class RocketUpdate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        deadTime -= Time.deltaTime;
         direction = new Vector3(0, -2, 0);
         direction *= speed * Time.deltaTime;
         transform.Translate(direction);
+
+        if(deadTime <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
