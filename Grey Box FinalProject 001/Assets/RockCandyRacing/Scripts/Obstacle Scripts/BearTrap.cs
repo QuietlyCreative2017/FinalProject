9106c8d6d619ff1a55560b07f063182e20766acd
Jasoncrawforddesign <jasoncrawford.design@gmail.com>
2017-11-13 11:22:45 +1100

    Added Bear Trap and updated player prefabs

---
+++ Grey Box FinalProject 001/Assets/RockCandyRacing/Scripts/Obstacle Scripts/BearTrap.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrap : MonoBehaviour
+{
+
+    public Animator anim;
+    bool falling = true;
+
+    void Awake()
+    {
+        //anim = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        falling = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, -Vector3.up, 3, LayerMask.NameToLayer("Ground")))
+        {
+            anim.SetBool("HasHitFloor", true);
+            falling = false;
+        }
+
+        if(falling)
+        {
+            Debug.DrawRay()
+            if(!Physics.Raycast(transform.position, -Vector3.up, 3, LayerMask.NameToLayer("Ground")))
+            {
+                transform.Translate(-Vector3.up * 0.5f);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            anim.SetBool("HasHitFloor", true);
+        }
+        if (col.gameObject.tag == "Player")
+        {
+            CharacterControls colControls = col.gameObject.GetComponent<CharacterControls>();
+            anim.SetBool("HasHitPlay", true);
+            colControls.Slow();
+        }
+    }
+}
